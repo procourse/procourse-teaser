9f4a6b03fe3606613addf169b6b0a789c58035ef
Justin DiRose <justin@justindirose.com>
2019-03-30 12:56:03 -0500

    FEATURE: SessionController tests [669]

---
+++ spec/requests/session_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe SessionController do
+  describe 'checking for teaser redirection cookies upon login' do
+    context 'when logging in locally' do
+      before(:each) do
+        cookies['teaser_url'] = '/t/100'
+        @user = Fabricate(:user)
+      end
+      it 'sets the destination_url cookie' do
+        post '/session.json', params: { login: @user.username, password:'myawesomepassword' }
+        expect(cookies['destination_url']).to eq('/t/100')
+      end
+      it 'removes the teaser_url cookie' do
+        post '/session.json', params: { login: @user.username, password:'myawesomepassword' }
+        expect(cookies['teaser_url']).to be_empty
+      end
+    end
+
+    let(:headers) { { host: Discourse.current_hostname } }
+    context 'when logging in via sso' do
+      before(:each) do
+        @sso_secret = 'abcdefg'
+        SiteSetting.sso_url = 'https://procourse.co'
+        SiteSetting.sso_secret = @sso_secret
+        SiteSetting.enable_sso = true
+        cookies['teaser_url'] = '/t/100'
+        @user = Fabricate(:user)
+      end
+      it 'removes the teaser_url cookie' do
+        get '/session/sso', params: { return_path: '/' } 
+        expect(cookies['teaser_url']).to be_empty
+      end
+    end
+  end
+end
